6efc5c45d9c5af2ea8260298ad446bc1d9651ce5
PedroHzzz <phpere85@gmail.com>
2022-08-22 17:11:19 -0300

    Adicionando Script do Desafio 1 - Linux Experience DIO

---
+++ Desafios/scriptdesafio1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Iniciando o Script..."
+
+echo "Criando Diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando Grupos de Usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando os usuários e definindo grupos..."
+
+useradd carlo -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_ADM
+useradd joaos -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -5 Senha123) -G GRP_SEC
+
+echo "Definindo as permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+echo "Atribuindo as permissões..."
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Script Finalizado..."
